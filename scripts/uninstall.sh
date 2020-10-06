c0374a644c8bb923d700dcb6a8f749033ec1091e
AlteredCoder <AlteredCoder>
2020-10-06 13:44:47 +0200

    fix uninstall script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-BIN_PATH_INSTALLED="/usr/local/bin/netfilter-blocker"
-CONFIG_DIR="/etc/crowdsec/netfilter-blocker/"
+BIN_PATH_INSTALLED="/usr/local/bin/firewall-bouncer"
+CONFIG_DIR="/etc/crowdsec/firewall-bouncer/"
 PID_DIR="/var/run/crowdsec/"
-SYSTEMD_PATH_FILE="/etc/systemd/system/netfilter-blocker.service"
+SYSTEMD_PATH_FILE="/etc/systemd/system/firewall-bouncer.service"
 
 uninstall() {
-	systemctl stop netfilter-blocker
+	systemctl stop firewall-bouncer
 	rm -rf "${CONFIG_DIR}"
 	rm -f "${SYSTEMD_PATH_FILE}"
-	rm -f "${PID_DIR}netfilter-blocker.pid"
+	rm -f "${PID_DIR}firewall-bouncer.pid"
 	rm -f "${BIN_PATH_INSTALLED}"
 }
 
 uninstall
+
+echo "firewall-bouncer uninstall successfully"
