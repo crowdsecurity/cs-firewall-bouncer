a3bc1745ad30d9887bb3f8fcaf58ce20ad1ef0ea
Marco Mariani <marco@crowdsec.net>
2021-12-22 09:53:48 +0100

    added log_err() to upgrade.sh

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,12 +2,20 @@
 BIN_PATH_INSTALLED="/usr/local/bin/crowdsec-firewall-bouncer"
 BIN_PATH="./crowdsec-firewall-bouncer"
 
+RED='\033[0;31m'
+NC='\033[0m'
 
 upgrade_bin() {
     rm "${BIN_PATH_INSTALLED}" || (echo "crowdsec-firewall-bouncer is not installed, exiting." && exit 1)
     install -v -m 755 -D "${BIN_PATH}" "${BIN_PATH_INSTALLED}"
 }
 
+
+log_err() {
+    msg=$1
+    date=$(date +%x:%X)
+    echo -e "${RED}ERR${NC}[${date}] crowdsec-firewall-bouncer: ${msg}" 1>&2
+}
 
 if ! [ $(id -u) = 0 ]; then
     log_err "Please run the upgrade script as root or with sudo"
