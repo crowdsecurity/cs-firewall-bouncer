9e7c0758da82821e6e288e414e38d3cdc704ccd5
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2022-02-17 13:54:09 +0530

    Add func tests for iptables
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- tests/nftables/test_nftables.py
+++ tests/nftables/test_nftables.py
@@ -1,57 +1,18 @@
 import json
 import os
 import subprocess
-import sys
 import unittest
 from ipaddress import ip_address
 from pathlib import Path
 from time import sleep
-from typing import List
 
-
-from tests.nftables.mock_lapi import MockLAPI
+from tests.mock_lapi import MockLAPI
+from tests.utils import generate_n_decisions, run_cmd
 
 SCRIPT_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 PROJECT_ROOT = SCRIPT_DIR.parent.parent
 BINARY_PATH = PROJECT_ROOT.joinpath("crowdsec-firewall-bouncer")
 CONFIG_PATH = SCRIPT_DIR.joinpath("crowdsec-firewall-bouncer.yaml")
-
-
-def run_cmd(cmd: List[str], trace_error=True):
-    p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    if trace_error and p.returncode:
-        raise SystemExit(
-            f"{cmd} exited with non-zero code with following logs:\n {p.stdout}"
-        )
-
-    return p.stdout
-
-
-def generate_n_decisions(n: int, action="ban", dup_count=0, ipv4=True, duration="4h"):
-    if dup_count >= n:
-        print("generate_n_decisions got dup_count>=n")
-        sys.exit(1)
-
-    unique_decision_count = n - dup_count
-    decisions = []
-    for i in range(unique_decision_count):
-        if ipv4:
-            ip = ip_address(i)
-        else:
-            ip = ip_address(2 ** 32 + i)
-        decisions.append(
-            {
-                "value": ip.__str__(),
-                "scope": "ip",
-                "type": action,
-                "origin": "script",
-                "duration": duration,
-                "reason": "for testing",
-            }
-        )
-    decisions += decisions[: n % unique_decision_count]
-    decisions *= n // unique_decision_count
-    return decisions
 
 
 class TestNFTables(unittest.TestCase):
@@ -69,7 +30,6 @@ class TestNFTables(unittest.TestCase):
         run_cmd(["nft", "delete", "table", "ip6", "crowdsec6"], trace_error=False)
 
     def test_table_rule_set_are_created(self):
-        sleep(1)
         output = json.loads(run_cmd(["nft", "-j", "list", "tables"]))
         tables = {
             (node["table"]["family"], node["table"]["name"])
---
+++ tests/utils.py
@@ -0,0 +1,38 @@
+import subprocess
+import sys
+from ipaddress import ip_address
+from typing import List
+
+
+def run_cmd(cmd: List[str], trace_error=True):
+    p = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    if trace_error and p.returncode:
+        raise SystemExit(f"{cmd} exited with non-zero code with following logs:\n {p.stdout}")
+
+    return p.stdout
+
+
+def generate_n_decisions(n: int, action="ban", dup_count=0, ipv4=True, duration="4h"):
+    if dup_count >= n:
+        raise SystemExit(f"generate_n_decisions got dup_count={dup_count} which is >=n")
+
+    unique_decision_count = n - dup_count
+    decisions = []
+    for i in range(unique_decision_count):
+        if ipv4:
+            ip = ip_address(i)
+        else:
+            ip = ip_address(2**32 + i)
+        decisions.append(
+            {
+                "value": ip.__str__(),
+                "scope": "ip",
+                "type": action,
+                "origin": "script",
+                "duration": duration,
+                "reason": "for testing",
+            }
+        )
+    decisions += decisions[: n % unique_decision_count]
+    decisions *= n // unique_decision_count
+    return decisions
