fb0454a971858ad5e3e799a019bf1e2789294718
Marco Mariani <marco@crowdsec.net>
2023-09-18 16:36:28 +0200

    func tests: no api key and certs at the same time

--- test/bouncer/test_tls.py
+++ test/bouncer/test_tls.py
@@ -1,5 +1,6 @@
 import json
 
+
 def test_tls_server(crowdsec, certs_dir, api_key_factory, bouncer, fw_cfg_factory):
     """TLS with server-only certificate"""
 
@@ -11,7 +12,7 @@ def test_tls_server(crowdsec, certs_dir, api_key_factory, bouncer, fw_cfg_factor
         'LAPI_KEY_FILE': '/etc/ssl/crowdsec/lapi.key',
         'USE_TLS': 'true',
         'LOCAL_API_URL': 'https://localhost:8080',
-        'BOUNCER_KEY_custom': api_key,
+        'BOUNCER_KEY_bouncer': api_key,
     }
 
     certs = certs_dir(lapi_hostname='lapi')
@@ -108,3 +109,45 @@ def test_tls_mutual(crowdsec, certs_dir, api_key_factory, bouncer, fw_cfg_factor
             assert bouncers[0]['name'].startswith('@')
             assert bouncers[0]['auth_type'] == 'tls'
             assert bouncers[0]['type'] == bouncer_under_test
+
+
+def test_api_key_and_cert(crowdsec, certs_dir, api_key_factory, bouncer, fw_cfg_factory):
+    """Attempt to send an api key and a certificate too"""
+
+    api_key = api_key_factory()
+
+    lapi_env = {
+        'CACERT_FILE': '/etc/ssl/crowdsec/ca.crt',
+        'LAPI_CERT_FILE': '/etc/ssl/crowdsec/lapi.crt',
+        'LAPI_KEY_FILE': '/etc/ssl/crowdsec/lapi.key',
+        'USE_TLS': 'true',
+        'LOCAL_API_URL': 'https://localhost:8080',
+        'BOUNCER_KEY_bouncer': api_key,
+    }
+
+    certs = certs_dir(lapi_hostname='lapi')
+
+    volumes = {
+        certs: {'bind': '/etc/ssl/crowdsec', 'mode': 'ro'},
+    }
+
+    with crowdsec(environment=lapi_env, volumes=volumes) as cs:
+        cs.wait_for_log("*CrowdSec Local API listening*")
+        cs.wait_for_http(8080, '/health', want_status=None)
+
+        port = cs.probe.get_bound_port('8080')
+        cfg = fw_cfg_factory()
+        cfg['api_url'] = f'https://localhost:{port}'
+        cfg['ca_cert_path'] = (certs / 'ca.crt').as_posix()
+        cfg['api_key'] = api_key
+
+        cfg['cert_path'] = (certs / 'bouncer.crt').as_posix()
+        cfg['key_path'] = (certs / 'bouncer.key').as_posix()
+
+        cs.wait_for_log("*Starting processing data*")
+
+        with bouncer(cfg) as cb:
+            cb.wait_for_lines_fnmatch([
+                "*Starting crowdsec-firewall-bouncer*",
+                "*unable to configure bouncer: api client init: cannot use both API key and certificate auth*",
+            ])
