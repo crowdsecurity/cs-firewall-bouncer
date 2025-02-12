d40a8adb15bd29d392a0ca975129cfbf60f8dd59
mmetc <92726601+mmetc@users.noreply.github.com>
2025-02-12 16:11:44 +0100

    CI: python tests with uv & typing (#393)
    basic layout & CI; more to come

--- test/pkg/test_build_rpm.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-pytestmark = pytest.mark.rpm
-
-
-def test_rpm_build(rpm_package, skip_unless_rpm):
-    """Test that the package can be built."""
-    assert rpm_package.exists(), f'Package {rpm_package} not found'
