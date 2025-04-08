eaa15751d007e03355467fce15f3b7d9f1922fb9
marco <marco@crowdsec.net>
2025-04-08 16:50:45 +0200

    lint: unexported-naming, unused-receiver

--- pkg/dryrun/dryrun.go
+++ pkg/dryrun/dryrun.go
@@ -16,31 +16,31 @@ func NewDryRun(_ *cfg.BouncerConfig) (types.Backend, error) {
 	return &dryRun{}, nil
 }
 
-func (d *dryRun) Init() error {
+func (*dryRun) Init() error {
 	log.Infof("backend.Init() called")
 	return nil
 }
 
-func (d *dryRun) Commit() error {
+func (*dryRun) Commit() error {
 	log.Infof("backend.Commit() called")
 	return nil
 }
 
-func (d *dryRun) Add(decision *models.Decision) error {
+func (*dryRun) Add(decision *models.Decision) error {
 	log.Infof("backend.Add() called with %s", *decision.Value)
 	return nil
 }
 
-func (d *dryRun) CollectMetrics() {
+func (*dryRun) CollectMetrics() {
 	log.Infof("backend.CollectMetrics() called")
 }
 
-func (d *dryRun) Delete(decision *models.Decision) error {
+func (*dryRun) Delete(decision *models.Decision) error {
 	log.Infof("backend.Delete() called with %s", *decision.Value)
 	return nil
 }
 
-func (d *dryRun) ShutDown() error {
+func (*dryRun) ShutDown() error {
 	log.Infof("backend.ShutDown() called")
 	return nil
 }
